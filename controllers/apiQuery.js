a47db291aa7b96c12fab5cda09d768cf25c56615
thechutrain <chu.chuyalan@gmail.com>
2017-03-26 00:44:51 -0400

    created findQuiz and addQuiz queries

--- controllers/apiQuery.js
+++ controllers/apiQuery.js
@@ -5,9 +5,15 @@ const db = require('../models');
 // const user_req_fields = ['username'];
 
 module.exports = {
+  // User Related Queries
   findUser: (id) => (id ? db.user.findOne({ where: { id }}) : db.user.findAll()),
-
   addUser: (userObj) => {
     return db.user.findOrCreate({ where: { username: userObj.username }, defaults: userObj });
-  }
+  },
+
+  // Quiz related queries
+  findQuiz: (id) => (id ? db.quiz.findOne({ where: { id }}) : db.quiz.findAll()),
+  addQuiz: (quizObj) => {
+    return db.quiz.findOrCreate({ where: { name: quizObj.name }, defaults: userObj });
+  } 
 }

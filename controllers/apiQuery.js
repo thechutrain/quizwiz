130601c8038076a121fd455210331e5fd6498263
thechutrain <chu.chuyalan@gmail.com>
2017-03-26 00:34:16 -0400

    added post route ... add validator

--- controllers/apiQuery.js
+++ controllers/apiQuery.js
@@ -1,7 +1,8 @@
 const db = require('../models');
 
 // validators
-const user_req_fields = ['username'];
+// TO DO LATER
+// const user_req_fields = ['username'];
 
 module.exports = {
   // test: function() {
@@ -11,13 +12,6 @@ module.exports = {
   findUser: (id) => (id ? db.user.findOne({ where: { id }}) : db.user.findAll()),
 
   addUser: (userObj) => {
-    // validate userObj
-    let valid = true;
-    userObjKeys = Object.keys(userObj);
-    user_req_fields.forEach((key) => {
-      if (userObjKeys.indexOf(key)){ valid = false };
-    });
-    if (!valid) { return Promise.resolve({ error: true, msg: 'Missing required fields' })};
     return db.user.findOrCreate({ where: { username: userObj.username }, defaults: userObj });
   }
 }
--- controllers/apiRouter.js
+++ controllers/apiRouter.js
@@ -1,23 +1,37 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-const query = require('./apiQuery');
+'use strict'
+const express = require('express')
+const router = express.Router()
+const query = require('./apiQuery')
 
 // ============ EXAMPLE ================
 router.get('/test', (req, res) => {
   query.test().then((results) =>{
-    res.json(results);
+    res.json(results)
   })
 })
 
 /** ========== Routes Related to User ============
  *  GET /user --> gets all users
  *  GET /user/:id --> gets a specified user based on their id
- * 
+ *  POST /user --> makes a new user
  */
-router.get('/', (req, res) => {
-  res.json({ 'test': true });
-});
+router.get('/user', (req, res) => {
+  query.findUser().then((users) => {
+    res.json(users)
+  })
+})
 
+router.get('/user/:id', (req, res) => {
+  query.findUser(req.params.id).then((user) => {
+    res.json(user)
+  })
+})
 
-module.exports = router;
+router.post('/user', (req, res) => {
+  // TO DO: VALIDATOR 
+  query.addUser(req.body).spread((user, created) => {
+    res.json(created);
+  })
+})
+
+module.exports = router

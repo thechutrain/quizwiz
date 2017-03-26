aa648b71bafec9ec95ab18e870ba434a167d093a
thechutrain <chu.chuyalan@gmail.com>
2017-03-26 01:54:03 -0400

    finished writing tests for the quiz

--- test/quiz.model.js
+++ test/quiz.model.js
@@ -20,13 +20,68 @@ describe(title, () => {
   it('should be an emptied quiz table', (done) => {
     query.findQuiz().then((results) => {
       try {
-        expect(results).to.be.a('array')
-        expect(results).to.have.lengthOf(0)
-        done()
+        expect(results).to.be.a('array');
+        expect(results).to.have.lengthOf(0);
+        done();
+      } catch(e) {
+        done(e);
+      }
+    })
+  }) // end emptied quiz table
+
+  it('should not return anything if you try to find a Non-existent user', (done) => {
+    query.findQuiz(-99).then((results) => {
+      try {
+        expect(results).to.be.a('null');
+        done();
       } catch(e) {
         done(e)
       }
     })
-  }) // end emptied quiz table
+  });
+
+  // QUIZ DATA
+  var name = 'testing quiz 123';
+  var description = 'this is a description of testing 123';
+
+  it('should be able to insert a new quiz', (done) => {
+    query.addQuiz({ name, description }).spread((result, created) => {
+      console.log(`New quiz??? ${created}`);
+      // console.log(result);
+      try {
+        expect(result).to.have.deep.property('dataValues.name', name);
+        expect(result).to.have.deep.property('dataValues.description', description);
+        assert.equal(created, true, 'should be true that it was created');
+        done();
+      } catch(e) {
+        done(e)
+      }
+    })
+  }) // closes it
+
+  it('won\'t create the same user again', (done) => {
+    query.addQuiz({ name, description }).spread((result, created) => {
+      console.log(`New user??? ${created}`);
+      try {
+        expect(result).to.have.deep.property('dataValues.name', name);
+        expect(result).to.have.deep.property('dataValues.description', description);
+        assert.equal(created, false, 'should be true that it was created');
+        done();
+      } catch(e){
+        done(e);
+      }
+    })
+  });
+
+  it('should be able to find the new quiz', (done) => {
+    query.findQuiz(1).then((result) => {
+      try {
+        expect(result).to.have.deep.property('dataValues.id', 1);
+        done();
+      } catch(e) {
+        done(e)
+      }
+    })
+  })
 
 })

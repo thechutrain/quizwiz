0544f9f59ce4d8e0bfab3a068440c780acf499cb
thechutrain <chu.chuyalan@gmail.com>
2017-03-27 17:03:07 -0400

    since not using bulk create, multiple insert queries dont guarantee order in Promise.all

--- test/query-tests/vote.model.js
+++ test/query-tests/vote.model.js
@@ -19,8 +19,13 @@ describe(title, () => {
     db.sequelize.sync({ force: true }).then(()=> {
       return Promise.all([
         query.addUser(user_a),
-        query.addUser(user_b),
+        // query.addUser(user_b),
         query.addQuiz(quiz_1),
+        // query.addQuiz(quiz_2)
+      ])
+    }).then(() => {
+      return Promise.all([
+        query.addUser(user_b),
         query.addQuiz(quiz_2)
       ])
     }).then(() => {
@@ -39,9 +44,18 @@ describe(title, () => {
       let first_user = resultArray[0][0].dataValues;
       let second_user = resultArray[0][1].dataValues;
       let first_quiz = resultArray[1][0].dataValues;
+      // console.log(first_quiz);
       let second_quiz = resultArray[1][1].dataValues;
+      // console.log('second quiz');
+      // console.log(second_quiz);
       try {
-        assert.equal(first_user, user_a, 'should be the same user');
+        // ?? is there a method to check object are the same??
+        assert.equal(first_user.username, user_a.username, 'should be the same username for user a');
+        assert.equal(first_user.password, user_a.password, 'should be the same password for user a');
+        assert.equal(second_user.username, user_b.username, 'should be the same username for user b');
+        assert.equal(second_user.password, user_b.password, 'should be the same password for user b');
+        assert.equal(first_quiz.name, quiz_1.name, 'should be the same quiz name for quiz 1');
+        assert.equal(first_quiz.description, quiz_1.description, 'should be the same description of quiz1');
         done();
       } catch(e) {
         done(e);

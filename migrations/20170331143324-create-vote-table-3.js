010eeb7a8667715a8d161fb4fd60845d71b27b0c
thechutrain <chu.chuyalan@gmail.com>
2017-03-31 10:47:27 -0400

    working vote table, fuuuk yaaaa

---
+++ migrations/20170331143324-create-vote-table-3.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable('vote', {
+      userId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'user',
+          key: 'id'
+        },
+      },
+      quizId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'quiz',
+          key: 'id'
+        },
+      },
+      stars: {
+        type: Sequelize.INTEGER,
+      }
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('vote');
+  }
+};

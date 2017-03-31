0c41c37edf5e447d8077f321fcc8a4e668f409fb
thechutrain <chu.chuyalan@gmail.com>
2017-03-31 10:59:39 -0400

    userQuiz looks good tooo ehhh yo

---
+++ migrations/20170331145117-createUserQuizTable4.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable('userquiz', 
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        },
+      },
+      quizId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'quiz',
+          key: 'id'
+        },
+      },
+      score: {
+        type: Sequelize.DECIMAL(5,2)
+      }
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('userquiz');
+  }
+};

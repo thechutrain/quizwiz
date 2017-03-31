156b8e9f183f545530b054f3101c85f08a5d73e4
thechutrain <chu.chuyalan@gmail.com>
2017-03-31 10:27:01 -0400

    migration to create user and quiz table

---
+++ migrations/20170331132826-create-user-table-1.js
@@ -0,0 +1,77 @@
+'use strict'
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable(
+       'user',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          createdAt: {
+            type: Sequelize.DATE
+          },
+          updatedAt: {
+            type: Sequelize.DATE
+          },
+          username: {
+            type: Sequelize.STRING,
+            allowNull: false
+          }
+          //foreign key usage
+          // attr4: {
+          //     type: Sequelize.INTEGER,
+          //     references: {
+          //         model: 'another_table_name',
+          //         key: 'id'
+          //     },
+          //     onUpdate: 'cascade',
+          //     onDelete: 'cascade'
+          // }
+        }
+      )
+    // return queryInterface.createTable('user', 
+    //     {
+    //       id: {
+    //         type: Sequelize.INTEGER,
+    //         primaryKey: true,
+    //         autoIncrement: true
+    //       },
+    //       createdAt: {
+    //         type: Sequelize.Date
+    //       },
+    //       updatedAt: {
+    //         type: Sequelize.Date
+    //       },
+    //       username: {
+    //         type: Sequelize.STRING,
+    //         allowNull: false,
+    //       },
+    //       isAdmin: {
+    //         type: Sequelize.BOOLEAN,
+    //         defaultValue: false
+    //       }
+    //     }
+    //   ) // ends createTable
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('user');
+  }
+}
---
+++ migrations/20170331135051-create-quiz-2.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable(
+    'quiz',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false
+        }, 
+        description: {
+          type: Sequelize.STRING
+        },
+        made_by: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'user',
+            key: 'id'
+          },
+          onUpdate: 'cascade',
+          onDelete: 'cascade'
+          }
+        }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('quiz');
+  }
+};

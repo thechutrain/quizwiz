38449a95b7e41830a14d52adcba1b06a20a26d84
thechutrain <chu.chuyalan@gmail.com>
2017-04-06 10:28:16 -0400

    cleaning up code

--- test/integration/userRouting.test.js
+++ test/integration/userRouting.test.js
@@ -1,17 +1,17 @@
 'use strict'
 /* global it, describe, before */
 const chai = require('chai')
-let dirtyChai = require('dirty-chai')
+const dirtyChai = require('dirty-chai')
 const assert = require('chai').assert
 const expect = require('chai').expect
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
 chai.use(dirtyChai)
-const server = require('../../server')
 
 // require the database models
 const models = require('../../models')
 const query = require('../../controllers/apiQuery')
+const server = require('../../server')
 
 const title =
 `
@@ -20,18 +20,7 @@ Integration Test '/user' routes
 ===============================
 `
 
-
-
 describe(title, () => {
-  // before(function () {
-  //   return new Promise((resolve, reject) => {
-  //     models.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', {raw: true}).then(() => {
-  //       models.sequelize.sync({ force: true }).then(() => {
-  //         resolve()
-  //       })
-  //     })
-  //   })
-  // })
   before(function () {
     return models.sequelize.sync({ force: true })
   })

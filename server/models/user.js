b8be8f27666413bca51821e4d180435fc66d9334
thechutrain <chu.chuyalan@gmail.com>
2017-04-29 11:50:42 -0400

    added bcrypt, and prehashing of password

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = function (sequelize, DataTypes) {
   var user = sequelize.define('user',
     // columns of table
@@ -21,6 +23,38 @@ module.exports = function (sequelize, DataTypes) {
         associate: function (models) {
           user.hasMany(models.userquiz)
           user.hasMany(models.vote)
+        },
+        hashPassword: function (inputPassword) {
+          return new Promise((resolve, reject) => {
+            bcrypt.genSalt(10)
+            .then((salt, error) => {
+              if (error) {
+                return reject(error)
+              }
+              return bcrypt.hash(inputPassword, salt)
+            })
+            .then((hash, error) => {
+              if (error) {
+                return reject(error)
+              }
+              return resolve(hash)
+            })
+          }) // ends new Promise
+        }
+      },
+      instanceMethods: {
+        comparePassword: function (inputPassword) {
+          return bcrypt.compareSync(inputPassword, this.password)
+        }
+      },
+      hooks: {
+        beforeCreateOptions: {},
+        beforeCreate: function (pt, beforeCreateOptions, done) {
+          const password = JSON.parse(JSON.stringify(pt)).password
+          return this.hashPassword(password).then((hash) => {
+            pt.password = hash
+            done()
+          })
         }
       }
     }) // end .define

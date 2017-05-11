b211f20c4bbf1adabb9ee65ca571c0caf63c4437
thechutrain <chu.chuyalan@gmail.com>
2017-05-11 12:14:48 -0400

    adding ssl true

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -10,7 +10,12 @@ var db = {}
 
 var sequelize
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable])
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  })
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
 }

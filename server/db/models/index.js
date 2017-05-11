3b6a5377940529196841afe202a4fdbcbdf25139
thechutrain <chu.chuyalan@gmail.com>
2017-05-11 13:56:38 -0400

    testing to see if its workign?

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -14,6 +14,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env.DATABASE_URL)
   // sequelize = new Sequelize(process.env[config.use_env_variable])
 } else {
+  console.log('======== NOT using the env var WTF? ==============')
   sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
 

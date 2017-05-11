5b57aea68004a0c679ebb044c47a91ecf1b16a91
thechutrain <chu.chuyalan@gmail.com>
2017-05-11 11:51:13 -0400

    getting ready for heroku deployment

---
+++ server/db/config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'development': {
+    'username': 'alanchu',
+    'password': '',
+    'database': 'quizwiz_db',
+    'host': '127.0.0.1',
+    'dialect': 'postgres'
+  },
+  'test': {
+    'username': 'alanchu',
+    'password': '',
+    'database': 'quizwiz_testing_db',
+    'host': '127.0.0.1',
+    'dialect': 'postgres',
+    'logging': false
+  },
+  'travisTest': {
+    'username': 'postgres',
+    'password': null,
+    'database': 'quizwiz_travis_db',
+    'host': '127.0.0.1',
+    'dialect': 'postgres'
+  },
+  'production': {
+    'use_env_variable': process.env.DATABASE_URL
+  }
+}

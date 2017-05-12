d35caad35c781a9bc3f9fb4a0509544fc41c3c9d
thechutrain <chu.chuyalan@gmail.com>
2017-05-12 16:56:27 -0400

    all unit tests for userquiz

--- test/unit/userquiz.test.js
+++ test/unit/userquiz.test.js
@@ -1,91 +1,122 @@
-// 'use strict'
-// /* global it, describe, before */
-// const assert = require('chai').assert
-// const expect = require('chai').expect
+'use strict'
+/* global it, describe, before */
+const assert = require('chai').assert
+const expect = require('chai').expect
 
-// const models = require('../../server/db/models')
-// const query = require('../../server/queryAPI/apiQuery')
+const models = require('../../server/db/models')
+const query = require('../../server/queryAPI/apiQuery')
 
-// const title =
-// `
-// ===============================
-// Unit test on "userquiz" model
-// ===============================
-// `
+const title =
+`
+===============================
+Unit test on "user quiz" model
+===============================
+`
 
-// // Define testing variables
-// let userTest = {
-//   username: 'I_am_a_test',
-//   password: 'incorrect'
-// }
+let quiz1 = {
+  title: 'US History Testing Quiz',
+  description: 'A quiz on the history of the United States',
+  madeBy: 1
+}
 
-// let quizTest = {
-//   title: 'US History Testing Quiz',
-//   description: 'A quiz on the history of the United States',
-//   madeBy: 1
-// }
+let user1 = {
+  username: 'I_am_a_test',
+  password: 'incorrect'
+}
 
-// let userQuiz = {
-//   userId: 1,
-//   quizId: 1,
-//   score: 89.12
-// }
+describe(title, () => {
+  before(() => {
+    return models.sequelize.sync({ force: true })
+    .then(() => {
+      // 1. Make FIND ALL queries into all tables
+      return Promise.all([
+        query.findAllUsers(),
+        query.findAllQuizzes(),
+        query.findAllVotes(),
+        query.findAllQuizzesTaken()
+      ])
+    })
+    .then((promiseArray) => {
+      // 2. check all queries to see if they are empty []
+      promiseArray.forEach((searchResult) => {
+        assert.deepEqual(searchResult, [])
+      })
+    })
+    .then(() => {
+      // 3. add data into other tables HERE
+      return Promise.all([
+        query.newUser(user1)
+      ])
+    })
+    .then((insertPromises) => {
+      // 4. check that all insertions worked!
+      assert.isTrue(insertPromises[0][1], 'user should be created')
+      // assert.isTrue(insertPromises[1][1], 'quiz should be created')
+      return query.newQuiz(quiz1)
+    })
+    .then((insertPromise) => {
+      assert.isTrue(insertPromise[0][1], 'quiz should be created')
+    })
+  })
 
-// let userQuizAgain = {
-//   userId: 1,
-//   quizId: 1,
-//   score: 94.32
-// }
+  /** ================== Actual Tests Begin Here ========================
+   *
+   */
+  it('should be able to enter a valid quiz', (done) => {
+    query.takeQuiz({
+      userId: 1,
+      quizId: 1,
+      score: 5
+    }).then((rawResult) => {
+      const result = JSON.parse(JSON.stringify(rawResult))
+      // console.log(result)
+      // console.log('========')
+      expect(result.id).to.equal(1)
+      expect(result).to.include.keys([
+        'id', 'userId', 'quizId', 'score', 'updatedAt', 'createdAt'
+      ])
+      done()
+    })
+  })
 
-// describe(title, () => {
-//   before(() => {
-//     return models.sequelize.sync({ force: true })
-//   })
+  it('should be able to take the quiz again', (done) => {
+    query.takeQuiz({
+      userId: 1,
+      quizId: 1,
+      score: 5
+    }).then((rawResult) => {
+      const result = JSON.parse(JSON.stringify(rawResult))
+      expect(result.id).to.equal(2)
+      expect(result).to.include.keys([
+        'id', 'userId', 'quizId', 'score', 'updatedAt', 'createdAt'
+      ])
+      done()
+    })
+  })
 
-//   it('should be an empty userquiz table', (done) => {
-//     query.findQuizzesTaken().then((results) => {
-//       try {
-//         assert.deepEqual(results, [])
-//         done()
-//       } catch (e) {
-//         done(e)
-//       }
-//     })
-//   })
+  it('should not be able to enter a quiz-taken with invalid user', (done) => {
+    query.takeQuiz({
+      userId: -1,
+      quizId: 1,
+      score: 5
+    }).then((rawResult) => {
+      const result = JSON.parse(JSON.stringify(rawResult))
+      // console.log(result)
+      // console.log('========')
+      assert.deepEqual(result, {error: 'Error! Need a valid user'}, 'Should not be able to enter quiz without valid user')
+      done()
+    })
+  })
 
-//   it('should be able to take a quiz', (done) => {
-//     // add user, then quiz, then userQuiz
-//     query.addUser(userTest).then(() => {
-//       return query.makeQuiz(quizTest)
-//     }).then(() => {
-//       return query.takeQuiz(userQuiz)
-//     }).then((result) => {
-//       let createdUserQuiz = result.dataValues
-//       assert.equal(createdUserQuiz.score, userQuiz.score, 'should be the same score')
-//       done()
-//     })
-//   })
-
-//   it('shoud be able to find the new quiz that was taken', (done) => {
-//     query.findQuizzesTaken(1).then((results) => {
-//       expect(results).to.have.lengthOf(1)
-//       // MORE TESTING CASES HERE?
-//       done()
-//     })
-//   })
-
-//   it('should be able to take another quiz', (done) => {
-//     query.takeQuiz(userQuizAgain).then((result) => {
-//       let createdUserQuiz = result.dataValues
-//       assert.equal(createdUserQuiz.score, userQuizAgain.score, 'should be the same score')
-//       done()
-//     })
-//   })
-
-//   it('should be able to find all the quizzes taken by a user', (done) => {
-//     query.findQuizzesTaken(1).then((results) => {
-//       expect(results).to.have.lengthOf(2)
-//       done()
-//     })
-//   })
-// })
+  it('should not be able to enter a quiz-taken with invalid quiz', (done) => {
+    query.takeQuiz({
+      userId: 1,
+      quizId: -1,
+      score: 5
+    }).then((rawResult) => {
+      const result = JSON.parse(JSON.stringify(rawResult))
+      assert.deepEqual(result, {error: 'Error! Need a valid quiz'}, 'Should not be able to enter quiz without valid quiz')
+      done()
+    })
+  })
+}) // ends describe

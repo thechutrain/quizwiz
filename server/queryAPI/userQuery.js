51c824b0b44d74bb8ae20bce6ce3ee6afd0da119
thechutrain <chu.chuyalan@gmail.com>
2017-05-11 21:48:55 -0400

    commenting out shit Im not using now

--- server/queryAPI/userQuery.js
+++ server/queryAPI/userQuery.js
@@ -1,19 +1,19 @@
-const db = require('../db/models')
+// const db = require('../db/models')
 
-module.exports = {
-  findUserById: (id) => {
-    return db.user.findOne({
-      where: { id },
-      include: [
-        { model: db.userquiz },
-        { model: db.vote }
-      ]
-    })
-  },
-  findAllUsers: () => {
-    return db.user.findAll()
-  },
-  addUser: (userObj) => {
-    return db.user.findOrCreate({ where: { username: userObj.username }, defaults: userObj })
-  }
-}
+// module.exports = {
+//   findUserById: (id) => {
+//     return db.user.findOne({
+//       where: { id },
+//       include: [
+//         { model: db.userquiz },
+//         { model: db.vote }
+//       ]
+//     })
+//   },
+//   findAllUsers: () => {
+//     return db.user.findAll()
+//   },
+//   addUser: (userObj) => {
+//     return db.user.findOrCreate({ where: { username: userObj.username }, defaults: userObj })
+//   }
+// }

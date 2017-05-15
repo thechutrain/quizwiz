369efb664096b143f5554381e5fed481cf8c1a41
thechutrain <chu.chuyalan@gmail.com>
2017-05-15 15:35:33 -0400

    added test for 3 routes on "quiz/"

--- test/integration/quizRouting.test.js
+++ test/integration/quizRouting.test.js
@@ -20,6 +20,17 @@ Integration Test '/quiz' routes
 ===============================
 `
 
+const user1 = {
+  username: 'Alan',
+  password: 'secret'
+}
+const quiz1 = {
+  title: 'US History Testing Quiz',
+  description: 'A quiz on the history of the United States',
+  madeBy: 1
+}
+
+
 describe(title, () => {
   before(() => {
     return models.sequelize.sync({ force: true })
@@ -40,10 +51,54 @@ describe(title, () => {
     })
     .then(() => {
     // 3. add data into other tables HERE
+      return query.newUser(user1)
+    }).then((newUserPromise) => {
+      assert.isTrue(newUserPromise[1], 'user shoudl be created')
     })
   })
 
   it('should be an empty list of quizzes @ GET "/quiz/all"', (done) => {
-    done()
+    chai.request(server)
+      .get('/api/v2/quiz/all')
+      .end((err, res) => {
+        expect(err).to.be.null()
+        expect(res).to.have.status(200)
+        assert.deepEqual(res.body, [])
+        // console.log(res.body)
+        done()
+      })
+  })
+
+  it('should be able to make a new quiz @ POST "/quiz/new"', (done) => {
+    chai.request(server)
+      .post('/api/v2/quiz/new')
+      .send(quiz1)
+      .end((err, res) => {
+        expect(err).to.be.null()
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('object')
+        assert.isTrue(res.body.created, 'should have created a new quiz')
+        expect(res.body.quiz.title).to.equal(quiz1.title)
+        expect(res.body.quiz).to.include.keys([
+          'id', 'title', 'description', 'madeBy', 'updatedAt', 'createdAt'
+        ])
+        // console.log(res.body)
+        done()
+      })
+  })
+
+  it('should be able to find the new quiz that was made @ GET "/quiz/id/1"', (done) => {
+    chai.request(server)
+      .get('/api/v2/quiz/id/1')
+      .end((err, res) => {
+        expect(err).to.be.null()
+        expect(res).to.have.status(200)
+        expect(res.body.title).to.equal(quiz1.title)
+        expect(res.body).to.include.keys([
+          'id', 'title', 'description', 'madeBy', 'updatedAt', 'createdAt'
+        ])
+        // console.log(res.body)
+        done()
+      })
   })
 })

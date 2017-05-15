d9101269cb8fcab0654ea52386c54e627ef46beb
Alan Chu <thechutrain@users.noreply.github.com>
2017-05-15 16:36:34 -0400

    V2 0 3 (#27)
    * cleaning up code
    
    * added eslint files in git ignore
    
    * getting ride of files that aren't being used
    
    * userquizzes in the findUserById will not be populated
    
    * commented out testing code for seeing if quizzestaken would be populated
    
    * cleaned up userRouting and cleaning up quizRouting
    
    * update the readme
    
    * added test for 3 routes on "quiz/"

---
+++ test/integration/quizRouting.test.js
@@ -0,0 +1,104 @@
+'use strict'
+/* global it, describe, before */
+const chai = require('chai')
+let dirtyChai = require('dirty-chai')
+const assert = require('chai').assert
+const expect = require('chai').expect
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+chai.use(dirtyChai)
+const server = require('../../server/server')
+
+// // require the database models
+const models = require('../../server/db/models')
+const query = require('../../server/queryAPI/apiQuery')
+
+const title =
+`
+===============================
+Integration Test '/quiz' routes
+===============================
+`
+
+const user1 = {
+  username: 'Alan',
+  password: 'secret'
+}
+const quiz1 = {
+  title: 'US History Testing Quiz',
+  description: 'A quiz on the history of the United States',
+  madeBy: 1
+}
+
+
+describe(title, () => {
+  before(() => {
+    return models.sequelize.sync({ force: true })
+    .then(() => {
+    // 1. Make FIND ALL queries into all tables
+      return Promise.all([
+        query.findAllUsers(),
+        query.findAllQuizzes(),
+        query.findAllVotes(),
+        query.findAllQuizzesTaken()
+      ])
+    })
+    .then((promiseArray) => {
+    // 2. check all queries to see if they are empty []
+      promiseArray.forEach((searchResult) => {
+        assert.deepEqual(searchResult, [])
+      })
+    })
+    .then(() => {
+    // 3. add data into other tables HERE
+      return query.newUser(user1)
+    }).then((newUserPromise) => {
+      assert.isTrue(newUserPromise[1], 'user shoudl be created')
+    })
+  })
+
+  it('should be an empty list of quizzes @ GET "/quiz/all"', (done) => {
+    chai.request(server)
+      .get('/api/v2/quiz/all')
+      .end((err, res) => {
+        expect(err).to.be.null()
+        expect(res).to.have.status(200)
+        assert.deepEqual(res.body, [])
+        // console.log(res.body)
+        done()
+      })
+  })
+
+  it('should be able to make a new quiz @ POST "/quiz/new"', (done) => {
+    chai.request(server)
+      .post('/api/v2/quiz/new')
+      .send(quiz1)
+      .end((err, res) => {
+        expect(err).to.be.null()
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('object')
+        assert.isTrue(res.body.created, 'should have created a new quiz')
+        expect(res.body.quiz.title).to.equal(quiz1.title)
+        expect(res.body.quiz).to.include.keys([
+          'id', 'title', 'description', 'madeBy', 'updatedAt', 'createdAt'
+        ])
+        // console.log(res.body)
+        done()
+      })
+  })
+
+  it('should be able to find the new quiz that was made @ GET "/quiz/id/1"', (done) => {
+    chai.request(server)
+      .get('/api/v2/quiz/id/1')
+      .end((err, res) => {
+        expect(err).to.be.null()
+        expect(res).to.have.status(200)
+        expect(res.body.title).to.equal(quiz1.title)
+        expect(res.body).to.include.keys([
+          'id', 'title', 'description', 'madeBy', 'updatedAt', 'createdAt'
+        ])
+        // console.log(res.body)
+        done()
+      })
+  })
+})

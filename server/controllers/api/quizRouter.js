bfc45908c48d2799df6874dbba4a1ee21491bf7a
thechutrain <chu.chuyalan@gmail.com>
2017-08-11 11:23:56 -0400

    added a route for posting a new question

--- server/controllers/api/quizRouter.js
+++ server/controllers/api/quizRouter.js
@@ -6,27 +6,51 @@ const validator = require('../middleware/validator')
 
 // =========== QUIZ routes ==================
 router.get('/id/:id', (req, res) => {
-  query.findQuizById(req.params.id).then((quiz) => {
-    res.json(quiz)
-  })
+	query.findQuizById(req.params.id).then(quiz => {
+		res.json(quiz)
+	})
 })
 
 router.get('/all', (req, res) => {
-  query.findAllQuizzes().then((quizzes) => {
-    res.json(quizzes)
-  })
+	query.findAllQuizzes().then(quizzes => {
+		res.json(quizzes)
+	})
 })
 
 /**
  * signed in users only.
  */
-router.post('/new',
-  validator(['title', {key: 'description', optional: true}, 'madeBy']),
-  (req, res) => {
-    query.newQuiz(req.body).then((resultArray) => {
-      const [quiz, created] = resultArray
-      res.json({ quiz, created })
-    })
-  }
+router.post(
+	'/new',
+	validator(['title', { key: 'description', optional: true }, 'madeBy']),
+	(req, res) => {
+		query.newQuiz(req.body).then(resultArray => {
+			const [quiz, created] = resultArray
+			res.json({ quiz, created })
+		})
+	}
 )
+
+// =========== Question routes ==================
+router.post('/:quizId/question/new', (req, res) => {
+	console.log(req.params.quizId)
+	const { question, correctAnswer, choices } = req.body
+	query
+		.addQuestion({
+			quizId: req.params.quizId,
+			question,
+			correctAnswer,
+			choices
+		})
+		.then(result => {
+			// console.log(result)
+			res.json(result)
+		})
+		.catch(error => {
+			console.log('ERROR!!!')
+			console.log(error)
+			res.json(error)
+		})
+})
+
 module.exports = router

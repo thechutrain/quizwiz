4a313559ca190d5d69344e622ff5a6e7fc5d2983
thechutrain <chu.chuyalan@gmail.com>
2017-08-11 10:54:36 -0400

    changed dataype from JSON to an array

--- server/db/models/question.js
+++ server/db/models/question.js
@@ -1,33 +1,34 @@
-module.exports = function (sequelize, DataTypes) {
-  var question = sequelize.define('question',
-    // columns
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      quizId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'quiz',
-          key: 'id'
-        }
-      },
-      question: { type: DataTypes.STRING, allowNull: false },
-      choices: { type: DataTypes.JSON, allowNull: false },
-      correctAnswer: { type: DataTypes.INTEGER, allowNull: false }
-    },
-      // options
-    {
-      freezeTableName: true,
-      classMethods: {
-        associate: function (models) {
-          // quiz.belongsToMany(models.user, { through: 'vote', foreignKey: 'quizId' })
-          question.belongsTo(models.quiz, {foreignKey: 'quizId'})
-        }
-      } // end classMethods
-    }
-    ) // end .define
-  return question
+module.exports = function(sequelize, DataTypes) {
+	var question = sequelize.define(
+		'question',
+		// columns
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			quizId: {
+				type: DataTypes.INTEGER,
+				references: {
+					model: 'quiz',
+					key: 'id'
+				}
+			},
+			question: { type: DataTypes.STRING, allowNull: false },
+			choices: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false },
+			correctAnswer: { type: DataTypes.INTEGER, allowNull: false }
+		},
+		// options
+		{
+			freezeTableName: true,
+			classMethods: {
+				associate: function(models) {
+					// quiz.belongsToMany(models.user, { through: 'vote', foreignKey: 'quizId' })
+					question.belongsTo(models.quiz, { foreignKey: 'quizId' })
+				}
+			} // end classMethods
+		}
+	) // end .define
+	return question
 }

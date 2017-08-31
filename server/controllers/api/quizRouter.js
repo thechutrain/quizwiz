47191d2994312356dd102a54944a92b2e16833fb
thechutrain <chu.chuyalan@gmail.com>
2017-08-31 12:49:48 -0400

    updated quizROuter with new quizQuery and questionQuery

--- server/controllers/api/quizRouter.js
+++ server/controllers/api/quizRouter.js
@@ -2,17 +2,19 @@
 const express = require('express')
 const router = express.Router()
 const query = require('../../queryAPI/apiQuery')
+const quizQuery = require('../../queryAPI/').quizQuery
+const questionQuery = require('../../queryAPI/').questionQuery
 const validator = require('../middleware/validator')
 
 // =========== QUIZ routes ==================
 router.get('/id/:id', (req, res) => {
-	query.findQuizById(req.params.id).then(quiz => {
+	quizQuery.findQuizById(req.params.id).then(quiz => {
 		res.json(quiz)
 	})
 })
 
 router.get('/all', (req, res) => {
-	query.findAllQuizzes().then(quizzes => {
+	quizQuery.findAllQuizzes().then(quizzes => {
 		res.json(quizzes)
 	})
 })
@@ -24,7 +26,7 @@ router.post(
 	'/new',
 	validator(['title', { key: 'description', optional: true }, 'madeBy']),
 	(req, res) => {
-		query.newQuiz(req.body).then(resultArray => {
+		quizQuery.createQuiz(req.body).then(resultArray => {
 			const [quiz, created] = resultArray
 			res.json({ quiz, created })
 		})
@@ -35,8 +37,8 @@ router.post(
 router.post('/:quizId/question/new', (req, res) => {
 	console.log(req.params.quizId)
 	const { question, correctAnswer, choices } = req.body
-	query
-		.addQuestion({
+	questionQuery
+		.createQuestion({
 			quizId: req.params.quizId,
 			question,
 			correctAnswer,
---
+++ server/queryAPI/questionQuery.js
@@ -0,0 +1,25 @@
+const db = require('../db/models')
+
+module.exports = {
+	createQuestion
+}
+
+/**
+*
+*/
+function createQuestion(questionObj) {
+	const { question, choices, correctAnswer, quizId } = questionObj
+	return findQuizById(quizId).then(result => {
+		if (!result) {
+			// not a valid quizId
+			return { error: 'Not a valid quizId' }
+		}
+		// check that userId is equal to madeBy
+		return db.question.create({ question, choices, correctAnswer, quizId })
+	})
+}
+
+// TODO - write an update query here
+// function updateQuestion(questionObj) {
+//   const
+// }

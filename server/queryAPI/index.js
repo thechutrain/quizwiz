028f3e75128c0e521aad51e3deff5a646a817cf5
thechutrain <chu.chuyalan@gmail.com>
2017-08-31 13:21:28 -0400

    imports all the queries

--- server/queryAPI/index.js
+++ server/queryAPI/index.js
@@ -2,9 +2,13 @@
 const userQuery = require('./userQuery')
 const userQuizQuery = require('./userQuizQuery')
 const quizQuery = require('./quizQuery')
+const questionQuery = require('./questionQuery')
+const voteQuery = require('./voteQuery')
 
 module.exports = {
 	userQuery,
 	userQuizQuery,
-	quizQuery
+	quizQuery,
+	questionQuery,
+	voteQuery,
 }

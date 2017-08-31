d3a84e2868d29235a8b1f468edc1aaa7d7991d05
thechutrain <chu.chuyalan@gmail.com>
2017-08-31 10:36:55 -0400

    changed name of v4 database and upgraded to v4 sequelize

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,42 +1,44 @@
 var match
 if (process.env.DATABASE_URL) {
-  match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+	match = process.env.DATABASE_URL.match(
+		/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/
+	)
 } else {
-  match = 'abcdefg'.split('') // junk array so I don't get error with undefined match[4]
+	match = 'abcdefg'.split('') // junk array so I don't get error with undefined match[4]
 }
 
 module.exports = {
-  'development': {
-    'username': 'alanchu',
-    'password': '',
-    'database': 'quizwiz_db',
-    'host': '127.0.0.1',
-    'dialect': 'postgres'
-  },
-  'test': {
-    'username': 'alanchu',
-    'password': '',
-    'database': 'quizwiz_testing_db',
-    'host': '127.0.0.1',
-    'dialect': 'postgres',
-    'logging': false
-  },
-  'travisTest': {
-    'username': 'postgres',
-    'password': null,
-    'database': 'quizwiz_travis_db',
-    'host': '127.0.0.1',
-    'dialect': 'postgres'
-  },
-  'production': {
-    'database': match[5],
-    'username': match[1],
-    'password': match[2],
-    'host': match[3],
-    'dialect': 'postgres',
-    'port': match[4],
-    dialectOptions: {
-      ssl: true
-    }
-  }
+	development: {
+		username: 'alanchu',
+		password: '',
+		database: 'quizwiz_v3_db',
+		host: '127.0.0.1',
+		dialect: 'postgres'
+	},
+	test: {
+		username: 'alanchu',
+		password: '',
+		database: 'quizwiz_testing_db',
+		host: '127.0.0.1',
+		dialect: 'postgres',
+		logging: false
+	},
+	travisTest: {
+		username: 'postgres',
+		password: null,
+		database: 'quizwiz_travis_db',
+		host: '127.0.0.1',
+		dialect: 'postgres'
+	},
+	production: {
+		database: match[5],
+		username: match[1],
+		password: match[2],
+		host: match[3],
+		dialect: 'postgres',
+		port: match[4],
+		dialectOptions: {
+			ssl: true
+		}
+	}
 }

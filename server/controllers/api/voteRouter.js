c28274d3a7753fa6ff0865ab382434f8a4868da2
thechutrain <chu.chuyalan@gmail.com>
2017-09-03 14:54:41 -0400

    added some notes, maybe I should combine routes?

--- server/controllers/api/voteRouter.js
+++ server/controllers/api/voteRouter.js
@@ -5,6 +5,8 @@ const validator = require('../middleware/validator')
 // Query here:
 const voteQuery = require('../../queryAPI/').voteQuery
 
+// NOTES - the post & update should almost be combined
+// into the same route right?
 router.post('/new',
   validator(['userId', 'quizId', 'stars']),
   (req, res) => {
